e8855a2bd9e23f03b56ef2c1272c96c619bfafda
DDSRem <1448139087@qq.com>
2026-02-22 15:24:53 +0800

    bump: p115disk & p123disk

--- plugins.v2/p123disk/__init__.py
+++ plugins.v2/p123disk/__init__.py
@@ -21,7 +21,7 @@ class P123Disk(_PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/DDSRem-Dev/MoviePilot-Plugins/main/icons/P123Disk.png"
     # 插件版本
-    plugin_version = "1.3.0"
+    plugin_version = "1.3.1"
     # 插件作者
     plugin_author = "DDSRem"
     # 作者主页
